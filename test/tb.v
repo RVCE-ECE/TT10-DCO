67c71d4598008adcec0ec9dcc4ed1ef42e7d6b1f
RVCE-ECE <shylashreen@rvce.edu.in>
2025-03-01 23:34:12 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -45,7 +45,7 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-	  .rst_n  (~rst_n)     // not reset
+	  .rst_n  (rst_n)     // not reset
   );
 assign ui_in = dco_code;
 	assign uo_out = dco_out;
